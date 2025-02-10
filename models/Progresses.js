bc91b83926912d0ddc8c708d44c94dd2201a48f6
Billionare <billionare@mac.local>
2025-02-10 11:21:02 +0500

    work properly

--- models/Progresses.js
+++ models/Progresses.js
@@ -1,23 +1,41 @@
-module.exports = (sequelize) => {
-    const Progress = sequelize.define('Progress', {
-      progress_id: {
-        type: Sequelize.INTEGER,
-        primaryKey: true,
-        autoIncrement: true,
-      },
-      status: {
-        type: Sequelize.STRING(50),
-        defaultValue: 'not_started',
-      },
-      completed_at: {
-        type: Sequelize.DATE,
-      },
-    }, {
-      tableName: 'progress',
-      timestamps: true,
-      createdAt: 'created_at',
-      updatedAt: 'updated_at',
-    });
-  
-    return Progress;
-  };
+const { DataTypes } = require('sequelize');
+const sequelize = require('../config/db');
+
+const Progress = sequelize.define('Progress', {
+  progress_id: {
+    type: DataTypes.INTEGER,
+    primaryKey: true,
+    autoIncrement: true,
+  },
+  user_id: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+    references: {
+      model: 'Users',
+      key: 'id',
+    },
+  },
+  lesson_id: {
+    type: DataTypes.INTEGER,
+    references: {
+      model: 'lessons',
+      key: 'id',
+    },
+  },
+  status: {
+    type: DataTypes.STRING(50),
+    allowNull: false,
+    defaultValue: 'not_started',
+  },
+  completed_at: {
+    type: DataTypes.DATE,
+    allowNull: true,
+  },
+}, {
+  tableName: 'progresses',
+  timestamps: true,
+  createdAt: 'created_at',
+  updatedAt: 'updated_at',
+});
+
+module.exports = {Progress};
